2bc6666fbd90986f234a6081577cf9aab6d2f395
andrepl <andrepleblanc@gmail.com>
2014-07-17 17:56:13 -0400

    add support for 1.7.10

---
+++ v1_7_R4/src/main/java/com/norcode/bukkit/portablehorses/v1_7_R4/HorseDeathHandler.java
@@ -0,0 +1,29 @@
+package com.norcode.bukkit.portablehorses.v1_7_R4;
+
+import com.norcode.bukkit.portablehorses.IHorseDeathHandler;
+import org.bukkit.entity.Horse;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Iterator;
+
+public class HorseDeathHandler implements IHorseDeathHandler {
+	public void handleHorseDeath(EntityDeathEvent event, ItemStack emptySaddle) {
+		Horse h = (Horse) event.getEntity();
+		Iterator<ItemStack> sit = event.getDrops().iterator();
+		ItemStack s = null;
+		boolean found = false;
+		ItemStack invSaddle = h.getInventory().getSaddle();
+		while (sit.hasNext()) {
+			s = sit.next();
+			if (s.equals(invSaddle)) {
+				sit.remove();
+				found = true;
+				break;
+			}
+		}
+		if (found) {
+			event.getDrops().add(emptySaddle);
+		}
+	}
+}
